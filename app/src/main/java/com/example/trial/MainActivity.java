7f352131cc6951a39593c0676659b2ac6c515a83
Mayuresh Hivarekar <0911.mayuresh@gmail.com>
2022-09-17 13:13:36 +0530

    Initial commit

---
+++ app/src/main/java/com/example/trial/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.trial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        CustomWebViewClient client = new CustomWebViewClient(this);
+        webView = findViewById(R.id.webView);
+        webView.setWebViewClient(client);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://mayuresh02.github.io/PortfolioWebsite/");
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event)
+    {
+        if(keyCode == KeyEvent.KEYCODE_BACK && this.webView.canGoBack())
+        {
+            this.webView.goBack();
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+}
+
+class CustomWebViewClient extends WebViewClient
+{
+    private Activity activity;
+
+    public CustomWebViewClient(Activity activity)
+    {
+        this.activity = activity;
+    }
+
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView webView, String url)
+    {
+        return false;
+    }
+
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView webView, WebResourceRequest request)
+    {
+        return false;
+    }
+}
